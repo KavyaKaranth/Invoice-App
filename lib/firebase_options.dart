a57a244198f85025ecc2a1f4fb833adf3592d38b
Kavyashree Karanth <kavyashreekaranth@gmail.com>
2025-08-19 19:34:29 +0530

    invoice deploy

---
+++ lib/firebase_options.dart
@@ -0,0 +1,16 @@
+// lib/firebase_options.dart
+
+import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
+
+class DefaultFirebaseOptions {
+  static FirebaseOptions get currentPlatform {
+    return const FirebaseOptions(
+      apiKey: "AIzaSyDaumSQwiWMRGSDxUC5QJKeQ3DV_vkBD6A",
+      authDomain: "invoice-generator-7ae88.firebaseapp.com",
+      projectId: "invoice-generator-7ae88",
+      storageBucket: "invoice-generator-7ae88.firebasestorage.app",
+      messagingSenderId: "592713258491",
+      appId: "1:592713258491:web:635927e0b87511e52fe75e",
+    );
+  }
+}
